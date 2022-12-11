1b12175354bba9898b3bd78df8dcf7542238d676
Dakshitha Perera <d42kw01f@gmail.com>
2022-12-11 17:39:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def firstPage():
     slit.markdown('***')
     slit.header('About Me: :+1:')
     slit.markdown('### Dakshitha Perera')
-    slit.write("SITE: [https://d42kw01f.github.io/](https://d42kw01f.github.io/)\nGitHub: [d42kw01f](https://github.com/d42kw01f)")
+    slit.markdown("SITE: [https://d42kw01f.github.io/](https://d42kw01f.github.io/)\nGitHub: [d42kw01f](https://github.com/d42kw01f)")
     
 
 def main(model_path):
